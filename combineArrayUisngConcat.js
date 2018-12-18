d7d97bf758d2c58874e6bd00c8fde5ef437ce885
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-18 13:47:55 +0000

    combine arrays using .concat

---
+++ combineArrayUisngConcat.js
@@ -0,0 +1,11 @@
+// Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original. The function should return the concatenated array.
+
+function nonMutatingConcat(original, attach) {
+  // Add your code below this line
+  let joinedArray = original.concat(attach);
+  return joinedArray;
+  // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+console.log(nonMutatingConcat(first, second));
