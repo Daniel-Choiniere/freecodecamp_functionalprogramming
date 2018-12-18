f36a36d0fabf63915039d755693d539e2351afbc
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-12-18 13:22:29 +0000

    used slice to return part of an array

--- returnArrayUsingSlice.js
+++ returnArrayUsingSlice.js
@@ -1,8 +1,10 @@
+// Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.
+
 function sliceArray(anim, beginSlice, endSlice) {
-  // Add your code below this line
+  
   let newArray = anim.slice(beginSlice, endSlice);
   return newArray;
-  // Add your code above this line
+  
 }
 var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 sliceArray(inputAnim, 1, 3);
